df58c53cbbfea5dc9d31efc819be4e5388b72de9
Kaustubh Hiware <hiwarekaustubh@gmail.com>
2020-09-06 12:53:50 +0530

    part 1

---
+++ src/components/comp.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Comp(props) {
+  return <h1>{props.compx}</h1>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,20 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Comp from "../components/comp"
 
 const IndexPage = () => (
   <Layout>
+  <Comp compx="asad" />
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Hi bitch</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <button class="primary-button">Click me</button>
+
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
